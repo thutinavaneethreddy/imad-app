f3cd1c99881bd4ce4a7c47215087cdd091e2b542
thutinavaneethreddy <thutinavaneethreddy@gmail.com>
2018-03-29 12:55:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+var element = getElementById('img');
+var marginLeft = 0;
+function moveRight(){
+    marginLeft +=5;
+    img.style.marginLeft = marginLeft;
+}
+element.onclick = function(){
+    var interval = setInterval(moveRight,50);
+}
