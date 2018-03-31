23922c43a0ed1c43181c328e20f5f98033437ca9
thutinavaneethreddy <thutinavaneethreddy@gmail.com>
2018-04-01 00:21:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,19 +33,32 @@ button.onclick = function(){
     };
 };
 
+
+
 //submit name
 var nameInput = document.getElementById('name');
-//var name = nameInput.value;
+var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //Make a request to the server and send the name
-    //capture the names and render the list
-    var s=document.getElementById('count');
-    var names=['name1','name2','name3'];
-    var list='';
-    for(var i=0;i<names.length;i++){
-        list+='<li>' + names[i]+ '</li>';
+    var request = new XMLHttpRequest();
+    request.open('GET','http://thutinavaneethreddy.imad.hasura-app.io/submit-name?name='+name);
+    request.send(null);
+    
+    request.onreadystatechange = function(){
+        if(request.readyState == XMLHttpRequest.DONE){
+            if(request.status==200){
+                var names=request.responseText;
+                names=names.parse(names);
+                var list='';
+                for(var i=0;i<names.length;i++){
+                list+='<li>' + names[i]+ '</li>';
+                }
+                var ul=document.getElementById('namelist');
+                ul.innerHTML=list;
+            }
+        }
     }
-    var ul=document.getElementById('namelist');
-    ul.innerHTML=list;
+    //capture the names and render the list
+    
 };
