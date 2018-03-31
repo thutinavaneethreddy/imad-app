c58f6a47d3427117f984f9ab5d8ac36e23f74b04
thutinavaneethreddy <thutinavaneethreddy@gmail.com>
2018-03-31 23:19:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,5 +38,13 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
-    
-}
+    //Make a request to the server and send the name
+    //capture the names and render the list
+    var names=['name1','name2','name3'];
+    var list='';
+    for(var i=0;i<names.length();i++){
+        list=list + '<li>' + names[i]+'</li>';
+    }
+    var ul=document.getElementById('namelist');
+    ul.innerHTML=list;
+};
